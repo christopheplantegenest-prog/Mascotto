d329eb73eb65b5dbfeb7bc6ed363895a08adf373
christopheplantegenest-prog <christophe.plantegenest@gmail.com>
2026-07-24 09:29:01 +0200

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -10,3 +10,5 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
+const auth = firebase.auth();
+const googleProvider = new firebase.auth.GoogleAuthProvider();
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mascotto-v15";
+const CACHE_NAME = "mascotto-v16";
 const ASSETS = [
   "./",
   "./index.html",
